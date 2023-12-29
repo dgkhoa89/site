37dc35b5b3946356617e77e4f5e3e7f29d1e975d
khoa.77pm <khoa.77pm@gmail.com>
2023-12-29 22:08:35 +0700

    chap04: move fortunes function to custom module

--- Meadowlark.js
+++ Meadowlark.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var fortunes = require('./lib/fortunes')
 
 // set up handlebars view engine
 var handlebars = require('express3-handlebars')
@@ -10,23 +11,12 @@ app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + '/public'));
 app.set('port', process.env.PORT || 3000);
 
-
-var fortunes = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple.",
-   ];
-
-
 app.get('/', function (req, res) {
     res.render('home');
 });
+
 app.get('/about', function (req, res) {
-    var randomFortune =
-    fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', { fortune: randomFortune });
+    res.render('about', { fortune: fortunes.getFortune() });
 });
 
 // custom 404 page
